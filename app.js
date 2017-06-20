d1d56c2ddd5f28f624e798c9ffea0a0a078be014
Than3s <vavra.dav@gmail.com>
2017-06-20 20:19:05 +0200

    budget controller module

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// Modules
+var budgetController = (function () {
+
+    var x = 23;
+
+    var add = function (a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function (b) {
+
+            console.log(add(b));
+        }
+    }
+})();
